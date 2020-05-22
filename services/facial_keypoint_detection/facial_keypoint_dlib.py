1e4f613d58300806a57be078b68889317fd163a4
thanit456 <too_w_o@hotmail.com>
2020-05-22 21:22:24 +0700

    stuck with figuring out the best structure of code

--- services/facial_keypoint_detection/facial_keypoint_dlib.py
+++ services/facial_keypoint_detection/facial_keypoint_dlib.py
@@ -12,16 +12,6 @@ class DLibFacialKeyPointDetector:
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor(shape_detector_path)
 
-    def detect(image):
-        image = imutils.resize(image, width=500)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        rects = self.detector(gray, 1)
-
-        for i, rect in enumerate(rects):
-            shape = self.predictor(gray, rect)
-            shape = shape2np(rect)
-
     def rect2bb(rect):
         x = rect.left()
         y = rect.top()
@@ -34,3 +24,15 @@ class DLibFacialKeyPointDetector:
         for i in range(0, 68):
             coords[i] = (shape.part(i).x, shape.part(i).y)
         return coords
+
+    def detect(image):
+        image = imutils.resize(image, width=500)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+        rects = self.detector(gray, 1)
+
+        for i, rect in enumerate(rects):
+            shape = self.predictor(gray, rect)
+            shape = shape2np(rect)
+
+        (x, y, w, h) = rect2bb(rect)
